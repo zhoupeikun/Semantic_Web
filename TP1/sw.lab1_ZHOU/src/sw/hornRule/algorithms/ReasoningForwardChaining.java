572a50329ff683e2c84852267d366eb36d8727aa
peikun <zpkhhx@gmail.com>
2016-09-30 23:28:47 +0200

    version1

--- TP1/sw.lab1_ZHOU/src/sw/hornRule/algorithms/ReasoningForwardChaining.java
+++ TP1/sw.lab1_ZHOU/src/sw/hornRule/algorithms/ReasoningForwardChaining.java
@@ -5,6 +5,9 @@ package sw.hornRule.algorithms;
 
 import sw.hornRule.models.FactBase;
 import sw.hornRule.models.Formalism;
+import sw.hornRule.models.HornRule;
+import sw.hornRule.models.HornRuleBase;
+import sw.hornRule.models.Variable;
 
 /**
  * @author  ZHOU Peikun
@@ -20,10 +23,59 @@ public class ReasoningForwardChaining extends AlogrithmChaining {
 	 */
 	//It's your turn to implement the algorithm, including the methods match() and eval()
 	public FactBase forwardChaining(Formalism ruleBase, Formalism factBase){
+		FactBase factBaseSet = new FactBase();
+		factBaseSet = (FactBase) factBase;
 		
-		return new FactBase();
+		HornRuleBase ruleBaseSet = (HornRuleBase) ruleBase;
+		
+		for(HornRule rule: ruleBaseSet.getRules()){
+			if (eval(rule, factBaseSet) == true) {
+				factBaseSet.setFact(rule.getConclusions());
+			}
+		}
+		
+/*		boolean eval(HornRule rule, FactBase factBase){
+			for(Variable condition: rule.getConditions()){
+				if (match(condition, factBase) == false) 
+						return false;
+				else 
+					return true;		
+				}
+			}*/
+			
+/*		boolean match(Variable condition, FactBase factBase){
+			for(Variable fact: factBase.getFact())
+				if (condition == fact)
+					return true;
+				else
+					continue;
+		}*/
+		
+		
+		return factBaseSet;
 	};
 	
+
+	private boolean eval(HornRule rule, FactBase factBase) {
+		// TODO Auto-generated method stub
+		for(Variable condition: rule.getConditions()){
+			if (match(condition, factBase) == false) 
+					return false;
+			else 
+				return true;		
+			}
+		return true;
+	}
+
+
+	private boolean match(Variable condition, FactBase factBase) {
+		// TODO Auto-generated method stub
+		if(!factBase.getFact().contains(condition))
+			return false;
+		else
+			return true;
+	}
+
 
 	public boolean entailment(Formalism ruleBase, Formalism factBase, Formalism query) {
 		FactBase allInferredFacts = forwardChaining(ruleBase, factBase);

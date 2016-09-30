89d04fda2b21f02f26b720913a33f75572df4057
peikun <zpkhhx@gmail.com>
2016-10-01 01:00:15 +0200

    version1

--- TP1/sw.lab1_ZHOU/src/sw/hornRule/algorithms/ReasoningBackwardChaining.java
+++ TP1/sw.lab1_ZHOU/src/sw/hornRule/algorithms/ReasoningBackwardChaining.java
@@ -3,10 +3,16 @@
  */
 package sw.hornRule.algorithms;
 
+import java.util.HashSet;
+
+import sw.hornRule.models.FactBase;
 import sw.hornRule.models.Formalism;
+import sw.hornRule.models.HornRule;
+import sw.hornRule.models.HornRuleBase;
+import sw.hornRule.models.Variable;
 
 /**
- * @author  <Your name>
+ * @author  ZHOU Peikun
  *
  */
 public class ReasoningBackwardChaining extends AlogrithmChaining {
@@ -19,8 +25,37 @@ public class ReasoningBackwardChaining extends AlogrithmChaining {
 	private boolean backwardChaining(Formalism ruleBase, Formalism factBase,
 			Formalism query) {
 		// TODO  To complete
-		return false;
+		FactBase factBaseSet = new FactBase();
+		factBaseSet = (FactBase) factBase;
+		HornRuleBase ruleBaseSet = (HornRuleBase) ruleBase;
+		if (match(query, factBaseSet.getFact())){
+			return true;
+		}
+		else{
+			for (HornRule rule: ruleBaseSet.getRules()){
+				if (match(query, ruleBaseSet.getConclusions())){
+					boolean bool = true;
+					int i = 1;
+					while(bool && i <= ruleBaseSet.getRules().size()){
+						bool = backwardChaining(rule, factBaseSet, ruleBaseSet);
+						i = i + 1;
+					}
+					if (bool = true){
+						return true;
+					}
+				}
+			}
+			return false;
+		}
 	}
+	
+	private boolean match(Formalism query, HashSet<Variable> hashSet) {
+		// TODO Auto-generated method stub
+		if (hashSet.contains(query))
+			return true;
+		else 
+			return false;
+		}
 
 	@Override
 	public int countNbMatches() {

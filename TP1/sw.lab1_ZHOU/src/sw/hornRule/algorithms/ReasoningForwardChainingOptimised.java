44348bc35dc6cd60d9322b2c697261e3698056b6
peikun <zpkhhx@gmail.com>
2016-10-02 21:30:25 +0200

    Optimised

--- TP1/sw.lab1_ZHOU/src/sw/hornRule/algorithms/ReasoningForwardChainingOptimised.java
+++ TP1/sw.lab1_ZHOU/src/sw/hornRule/algorithms/ReasoningForwardChainingOptimised.java
@@ -3,8 +3,14 @@
  */
 package sw.hornRule.algorithms;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 import sw.hornRule.models.FactBase;
 import sw.hornRule.models.Formalism;
+import sw.hornRule.models.HornRule;
+import sw.hornRule.models.HornRuleBase;
+import sw.hornRule.models.Variable;
 
 /**
  * @author  ZHOU Peikun
@@ -19,10 +25,55 @@ public class ReasoningForwardChainingOptimised extends AlogrithmChaining {
 	 */
 	public FactBase forwardChainingOptimise(Formalism ruleBase, Formalism factBase){
 		//It's your turn to implement the algorithm
-		return null;
+		FactBase factBaseSet = new FactBase();
+		factBaseSet = (FactBase) factBase;
+		
+		HornRuleBase ruleBaseSet = (HornRuleBase) ruleBase;
+		
+		for (Variable fact: factBaseSet.getFact()){
+			factBaseSet.setFact(Propagate(fact, ruleBase).getFact());
+		}
+			
+		return factBaseSet;
 	};
-
  
+	private FactBase Propagate(Variable fact, Formalism ruleBase) {
+		// TODO Auto-generated method stub
+		FactBase factSet;
+		HornRuleBase ruleBaseSet = (HornRuleBase) ruleBase;
+		if(ruleBaseSet.getConditions().contains(fact)){
+		for (HornRule rule: ruleBaseSet.getRules()){
+			HashSet<Variable> conditions = rule.getConditions()
+			for (Variable atomCondition: conditions){
+				if (match(atomCondition, rule.getConditions())){
+					conditions.remove(atomCondition);
+				}
+			}
+			if (conditions.isEmpty()){
+				ruleBaseSet.remove(rule);
+				factSet.setFact(rule.getConclusions());
+			}
+		}
+		FactBase factBaseSet = factSet;
+		for(Variable factatom: factSet.getFact()){
+			factBaseSet.setFact(Propagate(fact, ruleBase));
+		}
+		return factBaseSet;
+	}
+	}
+
+
+
+	private boolean match(Variable condition, FactBase factBase) {
+		// TODO Auto-generated method stub
+		if(!factBase.getFact().contains(condition))
+			return false;
+		else
+			return true;
+	}
+
+
+
 	public boolean entailment(Formalism ruleBase, Formalism factBase, Formalism query) {
 		FactBase allInferredFacts = forwardChainingOptimise(ruleBase, factBase);
 		if(allInferredFacts.getFact().contains(query))
